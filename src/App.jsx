f573b3ddf3678e2ba333a77de2370ec70c17bc93
fabricio-romero-126190 <kevinvarsoviat007@gmail.com>
2026-07-04 11:56:43 -0300

    subiendoInicio

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import InicioPage from "./pages/Inicio";
+import SobreNosotrosPage from "./pages/SobreNosotros";
+import ServiciosPage from "./pages/Servicios";
+import ContactosPage from "./pages/Contactos";
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<InicioPage />} />
+        <Route path="/sobre-nosotros" element={<SobreNosotrosPage />} />
+        <Route path="/servicios" element={<ServiciosPage />} />
+        <Route path="/contactos" element={<ContactosPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
