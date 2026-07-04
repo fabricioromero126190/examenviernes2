ae8518807b77a780e5a890a5a3e569437da48315
fabricio-romero-126190 <kevinvarsoviat007@gmail.com>
2026-07-04 17:27:07 -0300

    correcionparalink

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import InicioPage from "./pages/Inicio";
 import SobreNosotrosPage from "./pages/SobreNosotros";
 import ServiciosPage from "./pages/Servicios";
 import ContactosPage from "./pages/Contactos";
-import { HashRouter, Routes, Route } from "react-router-dom";
 export default function App() {
   return (
     <BrowserRouter>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://fabricioromero126190.github.io/examenviernes2",
+  base: "/examenviernes2/",
 });
