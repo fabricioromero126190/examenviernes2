45280779c727f3cba866bb4c32464aab0767ceed
fabricio-romero-126190 <kevinvarsoviat007@gmail.com>
2026-07-04 16:41:51 -0300

    corriengorutabasedelapagina

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/examenviernes2/",
+  base: "https://fabricioromero126190.github.io/examenviernes2",
 });
